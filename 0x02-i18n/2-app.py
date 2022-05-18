6b71f926d07809d9490cad78a538dbb8862b9fb7
afecego <afecego@gmail.com>
2022-05-17 21:01:58 -0500

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """instantiate the Babel object in your app. Store it in a module-level
 variable named babel"""
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -28,7 +28,8 @@ def index():
 
 @babel.localeselector
 def get_locale():
-    """determine the best match with our supported languages."""
+    """determine the best match with our supported languages
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
