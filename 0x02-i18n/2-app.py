7a289fe9c5edb484afe8a3a83a172a28027bcd32
afecego <afecego@gmail.com>
2022-05-18 13:10:16 -0500

    correction of localeselector

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -12,8 +12,11 @@ class Config(object):
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+configuration = Config()
+
+
 app = Flask(__name__)
-app.config.from_object('2-app.Config')
+app.config.from_object(configuration)
 babel = Babel(app)
 
 
