0188956dd4b1e8465f18c662da6025d88e14a04a
afecego <afecego@gmail.com>
2022-05-17 21:29:17 -0500

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -27,7 +27,7 @@ def index():
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """determine the best match with our supported languages
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
